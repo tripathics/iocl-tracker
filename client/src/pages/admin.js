e2c62a4198e7e2e84ff6bc0ec75a64f7fe7e812c
Chandrashekhar Tripathi <tripathics@outlook.com>
2022-12-26 14:02:22 +0530

    update

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -1,29 +1,29 @@
 import { React, useEffect, useState } from "react"
-import { useJsApiLoader, GoogleMap } from "@react-google-maps/api"
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api"
 import LayoutComponent from "../layout/layoutcomponent"
 
 const Admin = () => {
-  const [vehiclePositions, setVehiclePositions] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+  const [map, setMap] = useState(null);
+
+  const fetchLocations = async () => {
+    const response = await fetch('http://192.168.26.39:5000/vehicles');
+    if (!response.ok) {
+      alert(`An error occured: ${response.statusText}`) 
+      return;
+    }
+    const vehicles = await response.json();
+    setVehicles(vehicles);
+  }
 
   useEffect(() => {
-    const fetchLocations = async () => {
-      const response = await fetch('http://192.168.26.39:5000/vehicles');
-      if (!response.ok) {
-        alert(`An error occured: ${response.statusText}`) 
-        return;
-      }
-      const vehicles = await response.json();
-      setVehiclePositions(vehicles);
-      console.log(vehiclePositions);
-    }
-
     fetchLocations();
   }, [])
-
+  
   const center = { lat: 26.182808644471546, lng: 91.80385223005672 }
 
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyDz9cjpAun4rIs4B00tsYndFRPYOLUVoFc"
+    googleMapsApiKey: "AIzaSyDz9cjpAun4rIs4B00tsYndFRPYOLUVoFc",
   })
 
   if (!isLoaded) {
@@ -37,12 +37,26 @@ const Admin = () => {
   return (
     <>
       <LayoutComponent>
-      <h1>Vehicles live</h1>
-      <a href="/">Back to home</a>
-      <div style={{ height: "60vh" }}>
-        <GoogleMap center={center} zoom={15} mapContainerStyle={{
-          width: "100%", height: "100%"
-        }}>
+      <div style={{ height: "70vh" }}>
+        <GoogleMap 
+          center={center} 
+          zoom={15} 
+          options={{
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false
+          }}
+          mapContainerStyle={{
+            width: "100%", height: "100%"
+          }}
+          onLoad={map => setMap(map)}
+        >
+          {vehicles.map((vehicle, i) => (
+            <Marker key={i}
+              position={vehicle.pos.coords}
+              label={vehicle.driverName}
+            />
+          ))}
         </GoogleMap>
       </div>
       </LayoutComponent>
